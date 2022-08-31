1d3752264d2f34369dee9dcee9a0a386260ed9d0
dezzerlol <misha.6325@gmail.com>
2022-08-31 23:36:48 +0500

    final cors 3

--- src/constants/url.const.ts
+++ src/constants/url.const.ts
@@ -4,4 +4,4 @@ export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000'
 
-export const url = isDev ? `http://localhost:3000/api/trpc` : `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}/api/trpc`
+export const url = isDev ? `http://localhost:3000/api/trpc` : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
