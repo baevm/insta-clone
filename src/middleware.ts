1129c937c69fad3e56940884c1058874d8265692
dezzerlol <misha.6325@gmail.com>
2022-08-31 23:40:39 +0500

    final now.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,5 @@
 export { default } from 'next-auth/middleware'
-import Cors from 'cors'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
-const cors = Cors()
-
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-
-export function withCors(handler: NextApiHandler) {
-  return async (req: NextApiRequest, res: NextApiResponse) => {
-    await runMiddleware(req, res, cors)
-
-    return await handler(req, res)
-  }
-}
 
 export const config = { matcher: ['/'] }
