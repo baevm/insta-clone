5c669d2b850ea42dc1ad6dda48fe4e215eb00734
dezzerlol <misha.6325@gmail.com>
2022-08-31 23:27:25 +0500

    final cors

--- src/constants/url.const.ts
+++ src/constants/url.const.ts
@@ -1,7 +1,7 @@
+import { isDev } from '../utils/isDev'
+
 export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000'
 
-export const url = process.env.NEXT_PUBLIC_DEPLOY_URL
-  ? `https://insta-clone-roan.vercel.app/api/trpc`
-  : 'http://localhost:3000/api/trpc'
+export const url = isDev ? `http://localhost:3000/api/trpc` : `https://${process.env.VERCEL_URL}/api/trpc`
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,28 +1,31 @@
 import * as trpcNext from '@trpc/server/adapters/next'
 import { appRouter } from '../../../server/router/app.router'
 import { createContext } from '../../../server/createContext'
+import { withCors } from '../../../middleware'
 
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext,
+export default withCors(
+  trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext,
 
-  responseMeta(ctx) {
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Origin', '.brand.localhost')
-    ctx.ctx?.res?.setHeader('Access-Control-Request-Method', '*')
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Headers', '*')
+    responseMeta(ctx) {
+      ctx.ctx?.res?.setHeader('Access-Control-Allow-Origin', '.brand.localhost')
+      ctx.ctx?.res?.setHeader('Access-Control-Request-Method', '*')
+      ctx.ctx?.res?.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+      ctx.ctx?.res?.setHeader('Access-Control-Allow-Headers', '*')
 
-    if (ctx.ctx?.req?.method === 'OPTIONS') {
-      ctx.ctx?.res?.writeHead(200)
-    }
+      if (ctx.ctx?.req?.method === 'OPTIONS') {
+        ctx.ctx?.res?.writeHead(200)
+      }
 
-    return {
-      headers: ctx.ctx?.res?.getHeaders() as Record<string, string>,
-      statusCode: ctx.ctx?.res?.statusCode || 200,
-    }
-  },
+      return {
+        headers: ctx.ctx?.res?.getHeaders() as Record<string, string>,
+        statusCode: ctx.ctx?.res?.statusCode || 200,
+      }
+    },
 
-  onError({ error }) {
-    console.error(error)
-  },
-})
+    onError({ error }) {
+      console.error(error)
+    },
+  })
+)
