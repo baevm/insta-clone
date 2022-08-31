8b16fa138ecec9c26b711ce4941f1cc6b42dee53
dezzerlol <misha.6325@gmail.com>
2022-08-31 21:38:02 +0500

    maybe now?

--- src/constants/url.const.ts
+++ src/constants/url.const.ts
@@ -2,6 +2,6 @@ export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000'
 
-export const url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+export const url = process.env.NEXTAUTH_URL
+  ? `https://${process.env.NEXTAUTH_URL}/api/trpc`
   : 'http://localhost:3000/api/trpc'
