d7498e8b35db04c6230336340e72a7fcb34ce46a
dezzerlol <misha.6325@gmail.com>
2022-08-31 23:03:25 +0500

    cors fix 5

--- src/constants/url.const.ts
+++ src/constants/url.const.ts
@@ -3,5 +3,5 @@ export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   : 'http://localhost:3000'
 
 export const url = process.env.NEXT_PUBLIC_DEPLOY_URL
-  ? `https://${process.env.DEPLOY_URL}/api/trpc`
+  ? `https://insta-clone-roan.vercel.app/api/trpc`
   : 'http://localhost:3000/api/trpc'
