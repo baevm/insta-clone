e4296949a3acad206d60aa4582aa213a3617f6ee
dezzerlol <misha.6325@gmail.com>
2022-12-21 23:30:34 +0500

    cors error fix

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,24 +5,4 @@ import { createContext } from '../../../server/createContext'
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
-
-  responseMeta(ctx) {
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Origin', '.brand.localhost')
-    ctx.ctx?.res?.setHeader('Access-Control-Request-Method', '*')
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-    ctx.ctx?.res?.setHeader('Access-Control-Allow-Headers', '*')
-
-    if (ctx.ctx?.req?.method === 'OPTIONS') {
-      ctx.ctx?.res?.writeHead(200)
-    }
-
-    return {
-      headers: ctx.ctx?.res?.getHeaders() as Record<string, string>,
-      statusCode: ctx.ctx?.res?.statusCode || 200,
-    }
-  },
-
-  onError({ error }) {
-    console.error(error)
-  },
 })
